89fbdff8eac45355c3e8ce2259f161da261ac218
Kostiantyn Perehuda <kostiaperehuda@gmail.com>
2023-12-17 17:35:55 +0000

    Activity 6 - Summations

--- project-homework/src/interpreter.lua
+++ project-homework/src/interpreter.lua
@@ -15,8 +15,23 @@ local function to_hex_number_node(num)
     return to_number_node(num, 16)
 end
 
+-- Convert a list {n1, "+", n2, "+", n3, ...} into a tree
+-- {...{ operator = "+", left_operand = {operator = "+", left_operand = n1, right_operand = n2}, right_operand = n3}...}
+local function fold_left_into_binop_tree(lst)
+    local tree = lst[1]
+    for i = 2, #lst, 2 do
+        tree = {
+            tag = "binop",
+            left_operand = tree,
+            operator = lst[i],
+            right_operand = lst[i + 1],
+        }
+    end
+    return tree
+end
+
 ------------------------------------ Space -------------------------------------
-local space_token = lpeg.S(" \t\n")^0
+local space = lpeg.S(" \t\n")^0
 ------------------------------------ Number ------------------------------------
 local int_number_body = lpeg.R("09")^1
 
@@ -26,10 +41,14 @@ local hex_number_prefix = "0" * lpeg.S("xX")
 local int_number = -hex_number_prefix * lpeg.C(int_number_body) / to_int_number_node
 local hex_number =  hex_number_prefix * lpeg.C(hex_number_body) / to_hex_number_node
 
-local number_token = (int_number + hex_number) * space_token
+local number = (int_number + hex_number) * space
+---------------------------- Arithmetic Expressions ----------------------------
+local additive_operator = lpeg.C(lpeg.S("+-")) * space
+
+local sum = lpeg.Ct(number * (additive_operator * number)^0) / fold_left_into_binop_tree
 --------------------------------------------------------------------------------
 
-local grammar = space_token * number_token * -1
+local grammar = space * sum * -1
 
 -------------------------------------------------------- Parser --------------------------------------------------------
 
@@ -39,10 +58,30 @@ end
 
 ------------------------------------------------------- Compiler -------------------------------------------------------
 
-local function compile (ast)
-    if ast.tag == "number" then
-        return {"push", ast.val}
+local function add_opcode(state, opcode)
+    local code = state.code
+    code[#code + 1] = opcode
+end
+
+local opcode_from_operator = {["+"] = "add", ["-"] = "sub"}
+
+local function generate_code_from_expression(state, expression)
+    if expression.tag == "number" then
+        add_opcode(state, "push")
+        add_opcode(state, expression.val)
+    elseif expression.tag == "binop" then
+        generate_code_from_expression(state, expression.left_operand)
+        generate_code_from_expression(state, expression.right_operand)
+        add_opcode(state, opcode_from_operator[expression.operator])
+    else
+        error("invalid tree")
     end
+end
+
+local function compile(ast)
+    local state = { code = {} }
+    generate_code_from_expression(state, ast)
+    return state.code
 end
 
 ----------------------------------------------------- Interpreter ------------------------------------------------------
@@ -55,6 +94,12 @@ local function run (code, stack)
             pc = pc + 1
             top = top + 1
             stack[top] = code[pc]
+        elseif code[pc] == "add" then
+            stack[top - 1] = stack[top - 1] + stack[top]
+            top = top - 1
+        elseif code[pc] == "sub" then
+            stack[top - 1] = stack[top - 1] - stack[top]
+            top = top - 1
         else
             error("unknown instruction")
         end
